77a0804d9e6095ca29a7dd02c3bf9c3964a3d129
Lucas Bonomo <bonomo.lucas@gmail.com>
2023-05-19 09:33:50 -0300

    Initiall commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+/**
+ * 
+ * @param {*} req 
+ * @returns 
+ */
+function log(req) {
+    let data = {
+        'url': req.url,
+        'method': req.method,
+        'body': req.body
+    }
+    console.log(req.body)
+    return data
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.all('*', (req, res) => {
+    res.send(log(req))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
